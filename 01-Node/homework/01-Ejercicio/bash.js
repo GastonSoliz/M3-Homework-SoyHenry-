9091f8bbd10602c2ab228f56f48e422cbf9d685c
Gaston Soliz <gaston.e.soliz@gmail.com>
2023-09-04 23:14:19 -0300

    homework 1

---
+++ 01-Node/homework/01-Ejercicio/bash.js
@@ -0,0 +1,30 @@
+const process = require("process");
+const { Z_ASCII } = require("zlib");
+const commands = require("./commands/index.js");
+
+function print(output) {
+  process.stdout.write(output);
+  process.stdout.write("\nprompt > ");
+}
+
+function bash() {
+  process.stdout.write("prompt > ");
+  process.stdin.on("data", (data) => {
+    const args = data.toString().trim().split(" ");
+    var cmd = args.shift(); // cmd -> 'cd'    args -> [ 'commands', 'otra' ] <- join " "  'commands otra'
+    // console.log("--->", args)
+    // if(commands[cmd]){
+    if (commands.hasOwnProperty(cmd)) {
+      commands[cmd](print, args.join(" ")); // commands.pwd(print, 'commands otra' )
+    } else {
+      print(`command not found: ${cmd}`);
+    }
+    // print(args)
+  });
+}
+
+bash();
+module.exports = {
+  print,
+  bash,
+};
---
+++ 01-Node/homework/01-Ejercicio/commands/index.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+const utils = require("../utils/request");
+const process = require("process");
+
+function pwd(print) {
+  print(process.cwd());
+}
+
+function date(print) {
+  print(Date());
+}
+
+function echo(print, args) {
+  print(args);
+}
+
+function ls(print) {
+  fs.readdir(".", (error, files) => {
+    if (error) throw error;
+    else print(files.join(" "));
+  });
+}
+
+function cat(print, args) {
+  fs.readFile(args, "utf-8", (error, data) => {
+    if (error) throw error;
+    else print(data);
+  });
+}
+
+function head(print, args) {
+  fs.readFile(args, "utf-8", (error, data) => {
+    if (error) throw error;
+    else print(data.split("\n").shift().trim());
+  });
+}
+
+function tail(print, args) {
+  fs.readFile(args, "utf-8", (error, data) => {
+    if (error) throw error;
+    else print(data.split("\n").pop().trim());
+  });
+}
+
+function curl(print, args) {
+  utils.request(args, (error, response) => {
+    if (error) throw error;
+    else print(response);
+  });
+}
+
+module.exports = {
+  pwd,
+  date,
+  echo,
+  ls,
+  cat,
+  head,
+  tail,
+  curl,
+};
