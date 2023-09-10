a063585295a459f8d36d6ee772ebaf1cc75875ec
Gaston Soliz <gaston.e.soliz@gmail.com>
2023-09-10 18:46:08 -0300

    homework 5 actualizada

---
+++ 05-Express/homework/02-Proyecto/Client/src/redux/actions.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+// export function addFav(character) {
+//   return { type: "ADD_FAV", payload: character };
+// }
+export const addFav = (character) => {
+  const endpoint = "http://localhost:3001/rickandmorty/fav";
+  return (dispatch) => {
+    axios.post(endpoint, character).then(({ data }) => {
+      return dispatch({
+        type: "ADD_FAV",
+        payload: data,
+      });
+    });
+  };
+};
+
+// export function removeFav(id) {
+//   return { type: "REMOVE_FAV", payload: id };
+// }
+export const removeFav = (id) => {
+  const endpoint = "http://localhost:3001/rickandmorty/fav/" + id;
+  return (dispatch) => {
+    axios.delete(endpoint).then(({ data }) => {
+      return dispatch({
+        type: "REMOVE_FAV",
+        payload: data.id,
+      });
+    });
+  };
+};
+
+export function filterCards(gender) {
+  return { type: "FILTER", payload: gender };
+}
+
+export function orderCards(orden) {
+  return { type: "ORDER", payload: orden };
+}
+
+export function reset() {
+  return { type: "RESET" };
+}
